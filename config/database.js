2159b38a130a34725ccc89fcc507d07fbfc586f2
Johann <jjcreacion@gmail.com>
2025-01-29 17:47:47 -0400

    29/01/2025

--- config/database.js
+++ config/database.js
@@ -1,8 +1,17 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('tnb-db','root','',{
-    host: 'localhost',
-    dialect: 'mysql',
+const data_conf = {
+    username: process.env.USERNAME_MYSQL,
+    password: process.env.PASSWORD_MYSQL,
+    db_name: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT,
+}
+
+const sequelize = new Sequelize(data_conf.db_name,data_conf.username,data_conf.password,{
+    host: data_conf.host,
+    dialect: data_conf.dialect,
 });
 
 module.exports = sequelize;
