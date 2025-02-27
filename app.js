dd6e336e8ee5415e0c8c74ab2fc8f2996d59425b
Johann Gonzalez <dt.ait.programador2@cgesucre.gob.ve>
2025-02-27 11:53:55 -0400

    27/02/2025

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const db = require('./models');
 
 app.use(cors());
-
 app.use(express.json());
 
 const profileRoutes = require('./routers/profileRoutes');
@@ -19,10 +18,48 @@ app.get('/', (req, res) => {
     res.send('TNB Home!');
 });
 
-
-db.sequelize.sync().then(() => {
-    app.listen(3000, () => {
-        console.log('Servidor corriendo en http://localhost:3000');
-    });
+app.use('/users', async (req, res, next) => {
+    try {
+        await userRoutes(req, res, next);
+    } catch (error) {
+        console.error('Error en /users:', error);
+        res.status(500).send('Error interno del servidor');
+    }
 });
 
+app.use('/profile', async (req, res, next) => {
+    try {
+        await profileRoutes(req, res, next);
+    } catch (error) {
+        console.error('Error en /profile:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+app.use('/person', async (req, res, next) => {
+    try {
+        await personRoutes(req, res, next);
+    } catch (error) {
+        console.error('Error en /person:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+app.use((req, res, next)=>{
+    res.status(404).send("ruta no encontrada");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo salio mal!');
+});
+
+db.sequelize.sync().then(() => {
+    if (process.env.NODE_ENV !== 'production') {
+        app.listen(3000, () => {
+            console.log('Servidor corriendo en http://localhost:3000');
+        });
+    }
+});
+
+module.exports = app; 
