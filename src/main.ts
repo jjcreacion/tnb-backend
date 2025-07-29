930333d07eeb4b9d8d7b7baa4d65ca4ffa2f9d81
Johann <jjcreacion@gmail.com>
2025-07-28 21:13:36 -0400

    [Bug] Cords Websockets

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,72 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { ValidationPipe } from '@nestjs/common';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const allowedWebSocketOrigins = [
+    'http://localhost:3000',
+    'http://216.246.113.71:8080',
+    null,
+  ];
+
+  const ioAdapter = new IoAdapter(app);
+
+  const websocketPort = parseInt(process.env.PORT_WS || '12099', 10);
+  
+  ioAdapter.createIOServer(
+    websocketPort, 
+    {
+      cors: {
+        origin: allowedWebSocketOrigins,
+        methods: ['GET', 'POST'],
+        credentials: true,
+      },
+    }
+  );
+  app.useWebSocketAdapter(ioAdapter);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Documentación TNB-Backend')
+    .setDescription('Se busca facilitar la información de los endpoints disponibles para ...')
+    .setVersion('1.0')
+    .addTag('tag')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('doc', app, documentFactory);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    })
+  );
+
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  const appListenPort = parseInt(process.env.PORT || '12099', 10);
+  await app.listen(appListenPort); 
+}
+
+bootstrap();
+
+
+/*import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import * as process from "process";
 import { ValidationPipe } from "@nestjs/common";
 import { IoAdapter } from '@nestjs/platform-socket.io'; 
@@ -44,4 +110,4 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 5641);
 }
-bootstrap();
+bootstrap();*/
