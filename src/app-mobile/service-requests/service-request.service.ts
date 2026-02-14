98f87e7d50316df04e931aa7eb5b24527d992459
Johann González <jjcreacion@gmail.com>
2026-02-13 22:28:43 -0400

    Modificación del email de notificación

--- src/app-mobile/service-requests/service-request.service.ts
+++ src/app-mobile/service-requests/service-request.service.ts
@@ -32,6 +32,7 @@ export class RequestService {
         relations: [
           'fkUser',        
           'fkUser.person', 
+          'fkUser.person.phones',
           'fkCategory',    
           'fkSubCategory', 
           'fkStatus'
